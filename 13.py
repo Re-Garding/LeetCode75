a086811bafb32142a506d8db1a051600b48bea0d
Becky G <Beckygarding@gmail.com>
2023-01-31 17:34:56 -0700

    13,14

---
+++ 13.py
@@ -0,0 +1,40 @@
+"""
+Given an array of integers nums which is sorted in ascending order, and an integer target, write a function to search target in nums. If target exists, then return its index. Otherwise, return -1.
+
+You must write an algorithm with O(log n) runtime complexity.
+
+ 
+
+Example 1:
+
+Input: nums = [-1,0,3,5,9,12], target = 9
+Output: 4
+Explanation: 9 exists in nums and its index is 4
+Example 2:
+
+Input: nums = [-1,0,3,5,9,12], target = 2
+Output: -1
+Explanation: 2 does not exist in nums so return -1
+
+"""
+
+nums = [-1,0,3,5,9,12]
+tar = 9
+
+def search(nums, target):
+
+    low = 0
+    high = len(nums) -1
+    mid = 0
+
+    while low <= high:
+        mid = (high + low) // 2
+
+        if nums[mid] < target:
+            low = mid + 1
+
+        elif nums[mid] > target:
+            high = mid -1
+        else:
+            return mid
+    return -1
---
+++ completed/11.py
@@ -0,0 +1,35 @@
+"""
+Given the root of an n-ary tree, return the preorder traversal of its nodes' values.
+
+Nary-Tree input serialization is represented in their level order traversal. Each group of children is separated by the null value (See examples)
+
+ Example 1:
+
+Input: root = [1,null,3,2,4,null,5,6]
+Output: [1,3,5,6,2,4]
+Example 2:
+
+
+Input: root = [1,null,2,3,4,5,null,null,6,7,null,8,null,9,10,null,null,11,null,12,null,13,null,null,14]
+Output: [1,2,3,6,7,11,14,4,8,12,5,9,13,10]
+
+"""
+class Node:
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+
+def preorder(root):
+    if not root:
+        return []
+    values = []
+    to_visit = deque([root])
+
+    while to_visit:
+        cur = to_visit.popleft()
+        values.append(cur.val)
+        for c in reversed(cur.children):
+            to_visit.appendleft(c)
+    return values
+
+
